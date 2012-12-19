c0c073e2caf6403df03f368f8c5de043bb103155
Ryan Clark <rclark.sf@gmail.com>
2012-12-18 17:12:53 -0800

    First Commit

---
+++ Random/Random/RandomAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RandomAppDelegate.h
+//  Random
+//
+//  Created by Ryan Clark on 12/18/12.
+//  Copyright (c) 2012 Ryan Clark. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RandomAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Random/Random/RandomAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  RandomAppDelegate.m
+//  Random
+//
+//  Created by Ryan Clark on 12/18/12.
+//  Copyright (c) 2012 Ryan Clark. All rights reserved.
+//
+
+#import "RandomAppDelegate.h"
+
+@implementation RandomAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
